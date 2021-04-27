964dc47d1d0e9a31b7d61d3183ba0239b87ad08f
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-27 18:57:31 -0300

    Gerando bullets

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,8 @@ import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
 type Position = (Float, Float)
-type Velocity = (Float, Float)
-type Asteroid = (Position, Velocity, Picture)
-type Bullets = (Position, Velocity, Picture)
+type Velocity = Float
+type Object  = (Position, Velocity, Picture)
 
 -- Defining game elements
 data SpaceSurvivalGame = Game
@@ -15,7 +14,9 @@ data SpaceSurvivalGame = Game
   , shipHVelocity :: Float
   , asteroidPosition :: (Float, Float)
   , bulletPosition :: (Float, Float)
-  , asteroids :: [Asteroid]
+  , player :: Object
+  , bullets :: [Object]
+  , asteroids :: [Object]
   , paused :: Bool
   } deriving Show
 
@@ -26,47 +27,75 @@ initialState = Game
   , shipHVelocity = 0
   , asteroidPosition = (0, 250)
   , bulletPosition = (0,0)
-  , asteroids = []
+  , player = ((0,-250), 0, ship)
+  , bullets = [((0,0),0, bullet)]
+  , asteroids = [((0,250), 0, asteroid 5 5)]
   , paused = False  
   }
 
-
--- Draw all pictures at screen
-render :: SpaceSurvivalGame -> Picture
-render game = 
-    pictures [ mkShip $ shipLocation game
-             , asteroid (asteroidPosition game) 5 8
-             , bullet (bulletPosition game)]
-
-    where
-        mkShip :: Position -> Picture
-        mkShip (x,y) = translate x y $ color white $ lineLoop [(10,0), (0,25 ), (-10, 0), (9,0)]
-        
-        asteroid :: Position -> Float -> Float -> Picture
-        asteroid (x,y) w h= translate x y $ scale w h $color white $ lineLoop
+asteroid :: Float -> Float -> Picture
+asteroid w h = scale w h $ color white $ lineLoop
           [(1,5),(1,6),(2,4),(3,3),(4,3),(4,2),(3,2),(4,0),(3,-1),(2,-3),(0,-3),(-3,1),(-4,2),(-4,3),(-2,3),(1,5)]
 
-        bullet :: Position -> Picture
-        bullet (x,y) = translate x y $ color white $ rectangleSolid 3 5
+bullet :: Picture
+bullet = color white $ rectangleSolid 3 5
+
+ship :: Picture
+ship = color white $ lineLoop [(10,0), (0,25 ), (-10, 0), (9,0)]                
+
+-- Draw all pictures at screen
+drawObject :: Object -> Picture
+drawObject ((x,y), v, p) =
+  translate x y p
+
+drawGame :: [Object] -> Picture
+drawGame objects = 
+  pictures $ map drawObject objects        
+
+render :: SpaceSurvivalGame -> Picture
+render game = 
+    pictures [ drawObject (player game)            
+             , drawGame (bullets game)
+             , drawGame (asteroids game)]
 
 
 -- Keys configurations
 handleKeys :: Event -> SpaceSurvivalGame -> SpaceSurvivalGame
-handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _) game = game { shipHVelocity = -playerSpeed }
-handleKeys (EventKey (SpecialKey KeyLeft ) Up _ _) game = game {shipHVelocity = 0}
-handleKeys (EventKey (SpecialKey KeyRight ) Down _ _) game = game {shipHVelocity = playerSpeed}
-handleKeys (EventKey (SpecialKey KeyRight ) Up _ _) game = game {shipHVelocity = 0}
--- handleKeys (EventKey (SpecialKey KeySpace ) Down _ _) game = game {}
+handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _) game = game { player = updateVelocity (player game) (-playerSpeed)}
+handleKeys (EventKey (SpecialKey KeyLeft ) Up _ _) game = game {player = updateVelocity (player game) 0}
+handleKeys (EventKey (SpecialKey KeyRight ) Down _ _) game = game {player = updateVelocity (player game) playerSpeed}
+handleKeys (EventKey (SpecialKey KeyRight ) Up _ _) game = game {player = updateVelocity (player game) 0}
+handleKeys (EventKey (SpecialKey KeySpace ) Down _ _) game = bulletsGenerator game
 -- handleKeys (EventKey (SpecialKey KeyUp ) Up _ _) game = game {}
 -- handleKeys (EventKey (SpecialKey KeyDown ) Down _ _) game = game {}
 -- handleKeys (EventKey (SpecialKey KeyDown ) Up _ _) game = game {}
 handleKeys _ game = game
 
+bulletsGenerator :: SpaceSurvivalGame -> SpaceSurvivalGame
+bulletsGenerator game = game {bullets = (retrieveObjectPosition (player game), 0, bullet) : bullets game}
+
+retrieveObjectPosition :: Object -> Position
+retrieveObjectPosition ((x, y), v, p) = (x, y)
+
+retrieveVelocity :: Object -> Float
+retrieveVelocity ((x, y), v, p) = v
+
+updateVelocity :: Object -> Float -> Object
+updateVelocity  ((x, y), v, p ) velocity =  ((x, y), velocity, p )
+
+updatePositionX :: Object -> Float
+updatePositionX ((x, y), v, p)  = x + v
+
+updatePositionY :: Object -> Float
+updatePositionY ((x, y), v, p)  = y + v
+
 updatePlayerPosition :: SpaceSurvivalGame -> SpaceSurvivalGame
-updatePlayerPosition game = game {shipLocation = (limitMovement x' width 20,  y )}
+updatePlayerPosition game = game {player = ((limitMovement x' width 20,  y ), v, ship)}
                         where
-                          y = snd $ shipLocation game
-                          x' = shipHVelocity game + fst (shipLocation game)
+                          y = -250
+                          x' = updatePositionX (player game)
+                          v = retrieveVelocity( player game)
+
 
 -- Call the all functions and update the game
 update :: Float -> SpaceSurvivalGame -> SpaceSurvivalGame
