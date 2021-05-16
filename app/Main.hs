3b1186eeeb269efec450d3c47e10af211fb39855
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-05-16 14:10:20 -0300

    Random list added, Pause game working

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,10 @@ import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
     ( KeyState(Down, Up),
       SpecialKey(KeySpace, KeyLeft, KeyRight),
-      Key(SpecialKey),
+      Key(SpecialKey, Char),
       Event(EventKey) )
 import System.Random
+import RandomXpositions
 
 type Position = (Float, Float)
 type Velocity = Float
@@ -16,6 +17,8 @@ type Object  = (Position, Velocity, Picture)
 -- Defining game elements
 data SpaceSurvivalGame = Game
   { player :: Object
+  , lifes :: Float
+  , score :: Float
   , bullets :: [Object]
   , asteroids :: [Object]
   , paused :: Bool
@@ -25,20 +28,18 @@ data SpaceSurvivalGame = Game
 initialState :: SpaceSurvivalGame
 initialState = Game
   { player = ((0, -250), 0, ship)
+  , lifes = 3
+  , score = 0
   , bullets = [((0,0),0, bullet)]
-  , asteroids = [((0,250), -2, asteroid 5 5)]
+  , asteroids = [((head randomXpositions, 250), -2, asteroid 5 5)]
   , paused = False  
   }
-
-
-generateRandomValue :: (Random a, RandomGen b) => a -> a -> b -> a
-generateRandomValue l h g = do fst $ randomR (l, h) g                        
-
+            
 
 -- Asteroid Picture
 asteroid :: Float -> Float -> Picture
 asteroid w h = scale w h $ color white $ lineLoop
-          [(1,5),(1,6),(2,4),(3,3),(4,3),(4,2),(3,2),(4,0),(3,-1),(2,-3),(0,-3),(-3,1),(-4,2),(-4,3),(-2,3),(1,5)]
+          [(0, 4), (2, 2), (2, 0), (0, -2), (-1, -2), (-2, -1), (-2, 0), (-3, 1), (-3, 2), (-1, 4), (0, 4)]
 
 -- Bullet Picture
 bullet :: Picture
@@ -73,7 +74,7 @@ handleKeys (EventKey (SpecialKey KeyLeft ) Up _ _) game = game {player = updateV
 handleKeys (EventKey (SpecialKey KeyRight ) Down _ _) game = game {player = updateVelocity (player game) playerSpeed}
 handleKeys (EventKey (SpecialKey KeyRight ) Up _ _) game = game {player = updateVelocity (player game) 0}
 handleKeys (EventKey (SpecialKey KeySpace ) Down _ _) game = bulletsGenerator game
--- handleKeys (EventKey (SpecialKey KeyUp ) Up _ _) game = game {}
+handleKeys (EventKey (Char 'p'  ) Up _ _) game = game {paused = not $ paused game}
 -- handleKeys (EventKey (SpecialKey KeyDown ) Down _ _) game = game {}
 -- handleKeys (EventKey (SpecialKey KeyDown ) Up _ _) game = game {}
 handleKeys _ game = game
@@ -84,27 +85,49 @@ handleKeys _ game = game
 
 
 
-verifyPosition :: (Ord a1, Num a1) => a1 -> ((a2, a1), b, c) -> Bool
-verifyPosition lim ((x, y), v, p) 
+-- Verify object position and return if position is greater than limit
+verifyPositionY :: (Ord a1, Num a1) => a1 -> ((a2, a1), b, c) -> Bool
+verifyPositionY lim ((x, y), v, p) 
                                   | lim < 0 && y <= lim = False
                                   | lim > 0 && y >= lim = False
                                   | otherwise = True 
                                 
 
 verifyBulletPositionToDestroy :: SpaceSurvivalGame -> SpaceSurvivalGame
-verifyBulletPositionToDestroy game = game {bullets = filter (verifyPosition 250) (bullets game)  }
+verifyBulletPositionToDestroy game = game {bullets = filter (verifyPositionY 250) (bullets game)  }
 
 verifyAsteroidPositionToDestroy :: SpaceSurvivalGame -> SpaceSurvivalGame
-verifyAsteroidPositionToDestroy game = game {asteroids = filter (verifyPosition (-250)) (asteroids game)  }
+verifyAsteroidPositionToDestroy game = game {asteroids = filter (verifyPositionY (-250)) (asteroids game)  }
 
 
 -- Generate bullets at player position
 bulletsGenerator :: SpaceSurvivalGame -> SpaceSurvivalGame
-bulletsGenerator game = game {bullets = (retrieveObjectPosition (player game), 2, bullet) : bullets game}
+bulletsGenerator game = game {bullets = (sumInY (retrieveObjectPosition (player game)) (0,30), 2, bullet) : bullets game}                        
+
+sumInY :: Position-> Position -> Position
+sumInY (x,y) (x1,y1) = (x+x1,y+y1) 
 
 --Retrieve the position from a given object
 retrieveObjectPosition :: Object -> Position
 retrieveObjectPosition ((x, y), v, p) = (x, y)
+
+
+-- Update player lifes
+updatePlayerLifes :: Float -> Float -> Float
+updatePlayerLifes currentLife value = currentLife + value
+
+-- Update Score
+updateScore :: Float -> Float -> Float
+updateScore currentScore points = currentScore + points
+
+-- Verify if a collision happened
+collision :: Object -> Object -> Bool
+collision ((x1, y1), v1, p1) ((x2, y2), v2, p2) 
+                                             | x1 >= x2 -15 && x1 <= x2 + 15 && y1 >= y2 + 15 && y1 <= y2 - 15 = False
+                                             | otherwise = True
+
+-- verifyBulletVsAsteroidCollision :: SpaceSurvivalGame -> SpaceSurvivalGame
+-- verifyBulletVsAsteroidCollision game = game {bullets = filter collision ()}
 
 -- Retrieve the velocity of a given object
 retrieveVelocity :: Object -> Float
@@ -138,7 +161,6 @@ updateBulletPosition game = game {bullets = updateObjectPosition (bullets game)}
 
 updateAsteroidPosition :: SpaceSurvivalGame -> SpaceSurvivalGame
 updateAsteroidPosition game = game {asteroids = updateObjectPosition (asteroids game)}        
-
 
 -- Call the all functions and update the game
 update :: Float -> SpaceSurvivalGame -> SpaceSurvivalGame
@@ -182,4 +204,5 @@ fps = 60
 
 -- Main
 main :: IO ()
-main = play window background fps initialState render handleKeys update
+main = do
+  play window background fps initialState render handleKeys update
