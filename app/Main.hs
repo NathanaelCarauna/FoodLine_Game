359327efe07f3c9c987ffbc323933362b258c6c8
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-05-16 23:15:10 -0300

    Colisão funcionando parcialmente

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,9 @@ import Random
 
 type Position = (Float, Float)
 type Velocity = Float
-type Object  = (Position, Velocity, Picture)
+type Scale = Float
+type IsAlive = Bool
+type Object  = (Position, Velocity, Picture, Scale, IsAlive)
 
 -- Defining game elements
 data SpaceSurvivalGame = Game
@@ -30,17 +32,17 @@ data SpaceSurvivalGame = Game
 -- Initial game state
 initialState :: SpaceSurvivalGame
 initialState = Game
-  { player = ((0, -250), 0, ship)
+  { player = ((0, -250), 0, ship, 0, True)
   , lifes = 3
   , score = 0
   , bullets = []
   , indexRandomPosition = 0
   , indexRandomScale = 0
-  , asteroids = [((head randomXpositions, 250), -2, asteroid 15 15)]
+  , asteroids = []
   , time = 0
-  , paused = False  
-  }            
-            
+  , paused = False
+  }
+
 
 -- Asteroid Picture
 asteroid :: Float -> Float -> Picture
@@ -53,28 +55,28 @@ bullet = color white $ rectangleSolid 3 5
 
 -- Player ship Picture
 ship :: Picture
-ship = color white $ lineLoop [(10,0), (0,25 ), (-10, 0), (9,0)]                
+ship = color white $ lineLoop [(10,0), (0,25 ), (-10, 0), (9,0)]
 
 -- Draw an object on the screen
 drawObject :: Object -> Picture
-drawObject ((x,y), v, p) =
+drawObject ((x,y), v, p, s, a) =
   translate x y p
 
 -- Draw an Object list on the screen
 drawObjectList :: [Object] -> Picture
-drawObjectList objects = 
-  pictures $ map drawObject objects        
+drawObjectList objects =
+  pictures $ map drawObject objects
 
 -- Call all the drawers
 render :: SpaceSurvivalGame -> Picture
-render game | not (paused game) = pictures [ drawObject (player game)            
+render game | not (paused game) = pictures [ drawObject (player game)
                                                  , drawObjectList (bullets game)
                                                  , drawObjectList (asteroids game)
-                                                --  , drawObject ((-175,250),0, scale 0.1 0.1 $ color white $ Text $show (time game) )
+                                                 , drawObject ((-175,250),0, scale 0.1 0.1 $ color white $ Text $show (score game), 0, True )
                                             ]
-            |paused game = drawObject ((-175,0),0, scale 0.4 0.4 $ color white $ Text "Jogo Pausado")
-            |otherwise = drawObject ((-175,0),0, scale 0.4 0.4 $ color white $ Text "Game Over")
-    
+            |paused game = drawObject ((-175,0),0, scale 0.4 0.4 $ color white $ Text "Jogo Pausado", 0, True)
+            |otherwise = drawObject ((-175,0),0, scale 0.4 0.4 $ color white $ Text "Game Over",0, True)
+
 
 
 -- Keys configurations
@@ -91,35 +93,37 @@ handleKeys _ game = game
 
 
 asteroidsGenerator :: SpaceSurvivalGame -> SpaceSurvivalGame
-asteroidsGenerator game 
+asteroidsGenerator game
                         | indexRandomPosition game >= 600 = game {indexRandomPosition = 0}
                         | indexRandomScale game >= 45 = game {indexRandomScale = 0}
-                        | time game >= 60 = game
-                              { asteroids =  ((xPosition, 250), -2, asteroid scale scale) : asteroids game
-                              , indexRandomPosition = indexRandomPosition game + 1    
-                              , indexRandomScale = indexRandomScale game + 1      
-                              , time = 0                            
+                        | time game >= 100 = game
+                              { asteroids =  ((xPosition, 250), -2, asteroid scale scale, scale, True) : asteroids game
+                              , indexRandomPosition = indexRandomPosition game + 1
+                              , indexRandomScale = indexRandomScale game + 1
+                              , time = 0
                               }
-                        | otherwise = game            
+                        | otherwise = game
 
-              where 
+              where
                 xPosition =retrieveValueFromList (indexRandomPosition game) randomXpositions
-                scale = retrieveValueFromList (indexRandomScale game) randomScale 
+                scale = retrieveValueFromList (indexRandomScale game) randomScale
 
 clock :: SpaceSurvivalGame -> SpaceSurvivalGame
 clock game = game{time = time game + 1}
 
 retrieveValueFromList :: Int -> [Float] -> Float
-retrieveValueFromList index list = list !! index 
+retrieveValueFromList index list = list !! index
 
+retrieveScale :: Object -> Float
+retrieveScale ((x, y), v, p, s, a) = s
 
 -- Verify object position and return if position is greater than limit
-verifyPositionY :: (Ord a1, Num a1) => a1 -> ((a2, a1), b, c) -> Bool
-verifyPositionY lim ((x, y), v, p) 
+verifyPositionY :: (Ord a1, Num a1) => a1 -> ((a2, a1), b, c, s, a) -> Bool
+verifyPositionY lim ((x, y), v, p, s, a)
                                   | lim < 0 && y <= lim = False
                                   | lim > 0 && y >= lim = False
-                                  | otherwise = True 
-                                
+                                  | otherwise = True
+
 
 verifyBulletPositionToDestroy :: SpaceSurvivalGame -> SpaceSurvivalGame
 verifyBulletPositionToDestroy game = game {bullets = filter (verifyPositionY 250) (bullets game)  }
@@ -130,14 +134,14 @@ verifyAsteroidPositionToDestroy game = game {asteroids = filter (verifyPositionY
 
 -- Generate bullets at player position
 bulletsGenerator :: SpaceSurvivalGame -> SpaceSurvivalGame
-bulletsGenerator game = game {bullets = (sumInY (retrieveObjectPosition (player game)) (0,30), 2, bullet) : bullets game}                        
+bulletsGenerator game = game {bullets = (sumInY (retrieveObjectPosition (player game)) (0,30), 2, bullet, 2, True) : bullets game}
 
 sumInY :: Position-> Position -> Position
-sumInY (x,y) (x1,y1) = (x+x1,y+y1) 
+sumInY (x,y) (x1,y1) = (x+x1,y+y1)
 
 --Retrieve the position from a given object
 retrieveObjectPosition :: Object -> Position
-retrieveObjectPosition ((x, y), v, p) = (x, y)
+retrieveObjectPosition ((x, y), v, p, s, a) = (x, y)
 
 
 -- Update player lifes
@@ -148,67 +152,103 @@ updatePlayerLifes currentLife value = currentLife + value
 updateScore :: Float -> Float -> Float
 updateScore currentScore points = currentScore + points
 
--- Verify if a collision happened
-collision :: Object -> Object -> Bool
-collision ((x1, y1), v1, p1) ((x2, y2), v2, p2) 
-                                             | x1 >= x2 -15 && x1 <= x2 + 15 && y1 >= y2 + 15 && y1 <= y2 - 15 = False
-                                             | otherwise = True
 
--- verifyBulletVsAsteroidCollision :: SpaceSurvivalGame -> SpaceSurvivalGame
--- verifyBulletVsAsteroidCollision game = game {bullets = filter collision ()}
+-- Verify if a collisionB happened
+collisionB :: Object -> Object -> Object
+collisionB ((x1, y1), v1, p1, s1, a1) ((x2, y2), v2, p2, s2, a2)
+                                             | x1 >= x2 - s2 * raio && x1 <= x2 + s2 * raio && y1 >= y2 - s2 * raio = ((x1, y1), v1, p1, s1, False)
+                                             | otherwise = ((x1, y1), v1, p1, s1, True)
+                                          where 
+                                            raio = 3
+
+verifyCollisionB :: [Object] ->Object -> Object
+verifyCollisionB meteoros bala@((x1, y1), v1, p1, s1, a1) 
+                      | null meteoros = bala
+                      | a1 == False = ((x1, y1), v1, p1, s1, False)
+                      | a1 == True = foldl (collisionB) bala meteoros
+
+collisionA :: Object -> Object -> Object
+collisionA ((x1, y1), v1, p1, s1, a1) ((x2, y2), v2, p2, s2, a2)
+                                             | x1 - s1 * raio <= x2  && x1 + s1 * raio >= x2 && y1 - s1 * raio <= y2 = ((x1, y1), v1, p1, s1, False)
+                                             | otherwise = ((x1, y1), v1, p1, s1, True)
+                                      where
+                                        raio = 3
+
+verifyCollisionA :: [Object] ->Object -> Object
+verifyCollisionA balas meteoro@((x1, y1), v1, p1, s1, a1) 
+                      | null balas = meteoro
+                      | a1 == False = ((x1, y1), v1, p1, s1, False)
+                      | a1 == True = foldl (collisionA) meteoro balas
+
+
+
+
+verifyBulletVsAsteroidCollision :: SpaceSurvivalGame -> SpaceSurvivalGame
+verifyBulletVsAsteroidCollision game = game { 
+                                              bullets = filter retrieveIsAlive $ map (verifyCollisionB meteoros) balas
+                                            , asteroids = filter retrieveIsAlive $ map(verifyCollisionA balas) meteoros                                            
+                                            }
+                                      where                        
+                                          balas = bullets game
+                                          meteoros = asteroids game
+
+
+retrieveIsAlive :: Object -> Bool
+retrieveIsAlive ((x1, y1), v1, p1, s1, a1) = a1
 
 -- Retrieve the velocity of a given object
 retrieveVelocity :: Object -> Float
-retrieveVelocity ((x, y), v, p) = v
+retrieveVelocity ((x, y), v, p, s, a) = v
 
 updateVelocity :: Object -> Float -> Object
-updateVelocity  ((x, y), v, p ) velocity =  ((x, y), velocity, p )
+updateVelocity  ((x, y), v, p, s, a) velocity =  ((x, y), velocity, p,s,a )
 
 updatePositionX :: Object -> Float
-updatePositionX ((x, y), v, p)  = x + v
+updatePositionX ((x, y), v, p, s, a)  = x + v
 
 updatePositionY :: Object -> Float
-updatePositionY ((x, y), v, p)  = y + v
+updatePositionY ((x, y), v, p, s, a)  = y + v
 
 updateObject :: Object -> Object
-updateObject ((x, y), v, p) = ((x, y+v), v, p)
+updateObject ((x, y), v, p, s, a) = ((x, y+v), v, p,s, a)
 
 updateObjectPosition :: [Object] -> [Object]
 updateObjectPosition objs = map updateObject objs
 
 
 updatePlayerPosition :: SpaceSurvivalGame -> SpaceSurvivalGame
-updatePlayerPosition game = game {player = ((limitMovement x' width 20,  y ), v, ship)}
+updatePlayerPosition game = game {player = ((limitMovement x' width 20,  y ), v, ship, 0, True)}
                         where
                           y = -250
                           x' = updatePositionX (player game)
                           v = retrieveVelocity( player game)
 
 updateBulletPosition :: SpaceSurvivalGame -> SpaceSurvivalGame
-updateBulletPosition game = game {bullets = updateObjectPosition (bullets game)}              
+updateBulletPosition game = game {bullets = updateObjectPosition (bullets game)}
 
 updateAsteroidPosition :: SpaceSurvivalGame -> SpaceSurvivalGame
-updateAsteroidPosition game = game {asteroids = updateObjectPosition (asteroids game)}        
+updateAsteroidPosition game = game {asteroids = updateObjectPosition (asteroids game)}
 
 -- Call the all functions and update the game
 update :: Float -> SpaceSurvivalGame -> SpaceSurvivalGame
-update seconds game | not (paused game) = ( updatePlayerPosition 
+update seconds game | not (paused game) = ( updatePlayerPosition
                                           . updateBulletPosition
                                           . updateAsteroidPosition
                                           . verifyBulletPositionToDestroy
                                           . verifyAsteroidPositionToDestroy
                                           . clock
                                           . asteroidsGenerator
-                                          ) game                  
+                                          . verifyBulletVsAsteroidCollision
+                                          ) game
                     | otherwise = game
 
 limitMovement :: Float -> Int -> Float -> Float
 limitMovement move width playerWidth
         | move < leftLimit = leftLimit
-        | move > rightLimit = rightLimit 
+        | move > rightLimit = rightLimit
         | otherwise = move
         where
-          leftLimit = playerWidth/2 - fwidth/2 
+          leftLimit = playerWidth/2 - fwidth/2
           rightLimit = fwidth /2 - playerWidth/2
           fwidth = fromIntegral width :: Float
 
